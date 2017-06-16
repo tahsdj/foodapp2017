565f9dadab14a5507b2aa9443070b1e46a4e7705
yangyang95 <yangyang.weng@gmail.com>
2017-06-16 14:20:56 +0800

    Support Facebook login
    Check whether the user is login to FB and authorized
    when connect to login page.
    
    There are two situation:
    
    1. If authorized user is logged in, redirect to index page.
    2. If user is not logged in or authorized, provide login button.
       Redirect user to index page after login success.

---
+++ public/js/fb-login.js
@@ -0,0 +1,68 @@
+// Load the SDK asynchronously
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/zh_TW/sdk.js";
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+window.fbAsyncInit = function() {
+	FB.init({
+    appId      : '218300972010874',
+    cookie     : true,  // enable cookies to allow the server to access 
+                        // the session
+    xfbml      : true,  // parse social plugins on this page
+    version    : 'v2.8' // use graph api version 2.8
+  });
+
+  // Now that we've initialized the JavaScript SDK, we call 
+  // FB.getLoginStatus().  This function gets the state of the
+  // person visiting this page and can return one of three states to
+  // the callback you provide.  They can be:
+  //
+  // 1. Logged into your app ('connected')
+  // 2. Logged into Facebook, but not your app ('not_authorized')
+  // 3. Not logged into Facebook and can't tell if they are logged into
+  //    your app or not.
+  //
+  // These three cases are handled in the callback function.
+
+  FB.getLoginStatus(function(response) {
+	  statusChangeCallback(response);
+  });
+};
+
+// This is called with the results from from FB.getLoginStatus().
+function statusChangeCallback(response) {
+	console.log('statusChangeCallback');
+  console.log(response);
+  // The response object is returned with a status field that lets the
+  // app know the current login status of the person.
+  // Full docs on the response object can be found in the documentation
+  // for FB.getLoginStatus().
+  if (response.status === 'connected') {
+  	// Logged into your app and Facebook.
+    testAPI();
+    // Redirect
+    window.location = "/";
+  }
+}
+
+
+// Check status function
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
+
+
+// Here we run a very simple test of the Graph API after login is
+// successful.  See statusChangeCallback() for when this call is made.
+function testAPI() {
+	console.log('Welcome!  Fetching your information.... ');
+  FB.api('/me', function(response) {
+  	console.log('Successful login for: ' + response.name);
+	});
+}
